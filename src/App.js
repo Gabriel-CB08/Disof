064b53d440bf36b24cef4d2ec571c725a69028fc
Carlos <143907654+camendezd@users.noreply.github.com>
2023-10-06 20:34:14 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,33 @@ import RealTimeClock from './scr/RealTimeClock';
 function Header() {
   return (
     <header>
+            <nav class="navbar bg-dark" data-bs-theme="dark">
+        <div class="container-fluid">
+          <a class="navbar-brand">Navbar</a>
+          <ul class="nav nav-pills nav-fill">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/index.html">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/JAravena.html">Joaquín Aravena</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/GChomali.html">Gabriel Chomalí</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/CMendez.html">Carlos Méndez</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://github.com/Gabriel-CB08/DesSoft">GitHub</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+
       <h1>Header</h1>
     </header>
   );
